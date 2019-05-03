5d2eaf51e19ab67ed8be9c9bc756b7616372e1e0
Alejandro Sobrevilla <asobrevilla@msev.gob.mx>
2019-05-02 20:18:26 -0500

    Add project files.

---
+++ TablerosMiSEV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TablerosMiSEV.Startup))]
+namespace TablerosMiSEV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
