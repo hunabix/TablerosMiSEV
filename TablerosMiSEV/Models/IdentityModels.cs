cce37ab550b0c43809e886e7f26bde8cd5f5fa26
Alejandro Sobrevilla <asobrevilla@msev.gob.mx>
2019-05-02 21:13:23 -0500

    Update the DB name

--- TablerosMiSEV/Models/IdentityModels.cs
+++ TablerosMiSEV/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace TablerosMiSEV.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("TablerosMiSEVdb", throwIfV1Schema: false)
         {
         }
 
